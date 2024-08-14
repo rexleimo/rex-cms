0e1f28c14a91fdb71190d09a6dc35f3971a7d8f2
rex <rainbowmorel@163.com>
2024-08-14 18:05:43 +0800

    feat: refs mini add tailwind

--- rex-mini/src/pages/index/index.tsx
+++ rex-mini/src/pages/index/index.tsx
@@ -1,6 +1,18 @@
-import { View } from "@tarojs/components";
+import { View, Image } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
-import "./index.less";
+
+const imageList = [
+  "https://img0.baidu.com/it/u=1306108541,3890683509&fm=253&app=120&size=w931&n=0&f=JPEG&fmt=auto?sec=1723741200&t=9f34b107d0b6fdfacdcd27b974e7df0a",
+  "https://img2.baidu.com/it/u=1490602810,3032519025&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1723741200&t=1fdebbc149e1a04430fc781ab6a50bfa"
+]
+
+function ImageViewPort({ src }) {
+  return (
+    <View className='flex justify-center items-center'>
+      <Image src={src} className='w-full' />
+    </View>
+  )
+}
 
 export default function Index() {
   useLoad(() => {
@@ -8,8 +20,14 @@ export default function Index() {
   });
 
   return (
-    <View className='index-page'>
-      <View></View>
+    <View className='container'>
+      <View className='p-3'>
+        <View className='grid gap-3 grid-cols-2'>
+          {
+            imageList.map((src) => <ImageViewPort key={src} src={src} />)
+          }
+        </View>
+      </View>
     </View>
   );
 }
